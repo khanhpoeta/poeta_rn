7217e1e6e459420b0134679326d3c409ab475c90
Khanh Nguyen <khanh@poetadigital.com>
2023-04-16 18:21:20 +0700

    config project

---
+++ configuration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "start\n"
+if [ $1 != "" ] && [ $2 != "" ] && [ $3 != "" ]
+then
+  awk -v project="$2" '{sub("project_name", project)} {print}' $1 > temp.txt && mv temp.txt $3
+fi
--- src/plugins/createProject/plugin.ts
+++ src/plugins/createProject/plugin.ts
@@ -1,5 +1,6 @@
 import { execSync } from 'child_process';
-import fs from "fs";
+import fs from 'fs';
+import * as ejs from 'ejs';
 import { ProjectType } from '../chooseProjectType/_prompts';
 
 export async function apply(value: any, previousValue: any):Promise<void> {
@@ -10,22 +11,38 @@ export async function apply(value: any, previousValue: any):Promise<void> {
     fs.lstatSync(`${currentDirectory}/packages`).isDirectory();
   }
   catch {
-    await execSync(`cp -r ${appRoot.path+'/template/'} ${currentDirectory}`, { stdio: 'pipe' });
+    await execSync(`cp -r ${appRoot.path+'/template/workspace'} ${currentDirectory}`, { stdio: 'pipe' });
   }
 
   const projectType = previousValue as ProjectType;
+
+  const projectRootFolder = (path:string) =>{
+    return `${appRoot.path}/template/project/${path}`;
+  }
+
+  const currentProjectFolder = (path:string) =>{
+    return `${currentDirectory}/${value}/${path}`;
+  }
+  const podFile = 'ios/Podfile';
+  const manifest = 'android/app/src/main/AndroidManifest.xml';
+  const gradle = 'android/app/build.gradle';
+
   switch(projectType)
   {
     case ProjectType.native:
       await execSync(`npx react-native@latest init ${value} --template git+ssh://git@bitbucket.org:poetaadmin/codebase.mobile.git#master`, { stdio: 'inherit' });
       break;
     case ProjectType.web:
-      console.log('Comming soon ...');
+      
+      await execSync(`
+      bash configuration.sh ${projectRootFolder(podFile)} ${value} ${currentProjectFolder(podFile)} &&
+      bash configuration.sh ${projectRootFolder('android/AndroidManifest.xml')} ${value} ${currentProjectFolder(manifest)} &&
+      bash configuration.sh ${projectRootFolder('android/build.gradle')} ${value} ${currentProjectFolder(gradle)}`, { stdio: 'inherit' });
       break;
   }
-
-  
  
   return Promise.resolve();
 }
+
+
 
