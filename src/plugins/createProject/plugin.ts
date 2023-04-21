b083be050cd8522a18526adf29116c5c86c18a7a
Khanh Nguyen <khanh@poetadigital.com>
2023-04-21 14:47:37 +0700

    update package

--- src/plugins/createProject/plugin.ts
+++ src/plugins/createProject/plugin.ts
@@ -75,7 +75,7 @@ export async function apply(value: any, previousValue: any):Promise<void> {
         scripts[`${value}:pods`] = `yarn workspace ${value} pods`;
         scripts[`${value}:start`] = `[  -z "$env" ] && env=qc yarn workspace ${value} start || env=$env yarn workspace ${value} start`;
         scripts[`${value}:ios`] = `[  -z "$env" ] && env=qc yarn workspace ${value} ios || env=$env yarn workspace ${value} ios`;
-        scripts[`${value}:android`] = `[  -z "$env" ] && env=qc yarn workspace abcd android $env || env=$env yarn workspace abcd android $env`;
+        scripts[`${value}:android`] = `[  -z "$env" ] && env=qc yarn workspace ${value} android $env || env=$env yarn workspace ${value} android $env`;
         fs.writeFile(`${currentDirectory}/package.json`, JSON.stringify(packageJson, null, 2), (err) => {
             resolve();
         });
